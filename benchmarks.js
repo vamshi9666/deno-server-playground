d8a138d5f18ac2e8de7e750b3a4cb376fca775e7
vamshi9666 <vamshi9666@riseup.net>
2022-12-18 21:33:21 +0530

    fix; console added

--- benchmarks.js
+++ benchmarks.js
@@ -2,7 +2,8 @@ const autocannon = require("autocannon");
 
 async function foo() {
   const result = await autocannon({
-    url: "http://localhost:8000",
+    // url: "http://localhost:8000",
+    url: "https://vamshi9666-deno.deno.dev/",
     // connections: 10, //default
     pipelining: 1, // default
     // duration: 1, // default
@@ -12,3 +13,25 @@ async function foo() {
 }
 
 foo();
+
+async function run() {
+  const got = await (await import("got")).default;
+  const fs = require("fs");
+  // initate got instance
+
+  const api = got.extend({
+    prefixUrl: "https://vamshi9666-deno.deno.dev/",
+  });
+  // make 100 requests in parallel in 1 second within loop
+  const requests = new Array(10000).fill(api.get("api").text());
+
+  // wait for all requests to complete
+  const responses = await Promise.all(requests);
+
+  // print the response body
+  // console.log(responses);
+  // save the response body to a file
+  fs.writeFileSync("response.json", JSON.stringify(responses, null, 2));
+}
+
+run();
